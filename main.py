97797e92cb250a01059b063d766463253c2c28a5
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-11 23:10:56 +0000

    Work in progress. Working on figuring out JSON stuff in main.py. Also, I successfully added code in survey.html to generate unique id and name attributes for survey input data.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, request
 from datetime import datetime
 import pytz
 import random
-import csv
+import json
 
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 
 
 # DARK MODE AUTO-ASSIGN
@@ -45,32 +45,47 @@ def analytics():
 
 @app.route("/survey", methods=['GET', 'POST'])
 def survey():
+    chores = [
+        "kitchen",
+        "sweeping",
+        "laundry",
+        "bathrooms",
+        "trash",
+        "mowing",
+        "gardening",
+        "cooking",
+        "mirrors",
+        "decluttering",
+        "clening_cars",
+        "pets_care"
+    ]
+    
     if request.method == "POST":
+        # Handle receiving chores responses
         name = request.form['name_input']
         importance = request.form.getlist('importance_select')
         competence = request.form.getlist('competence_select')
         comfort = request.form.getlist('comfort_select')
         # Your code to process the form data goes here
-        # Check if the data already exists in the CSV file
-        with open('data/responses.csv', 'r') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                if row[0] == name:
+        # Check if the data already exists in the JSON file
+        with open('data/responses.json') as f:
+            data = json.load(f)
+            for response in data:
+                if response["name"] == name:
                     return 'Name already exists'
-        # Write the user's responses to a CSV file
-        with open('data/responses.csv', 'a', newline='') as f:
-            fieldnames = ['name', 'importance', 'competence', 'comfort']
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-    
-            data = {
-                'name': name,
-                'importance': importance,
-                'competence': competence,
-                'comfort': comfort
-            }
-    
-            writer.writerow(data)
-    
+        # Write the user's responses to a JSON file
+        chores_data = {}
+        for chore in chores:
+            chore_reactions = "chore:[]"
+            chores_data = chores_data.append(chore_reactions)
+            
+        new_response = [name, chores_data]
+        
+        data["responses"].append(new_response)
+        
+        with open('data/responses.json', 'w') as f:
+            json.dump(data, f, indent=4)
+        
         return 'Thanks for submitting your survey!'
 
         # Delete this later
@@ -83,30 +98,14 @@ def survey():
         return "Text saved!"
         
     mode = get_mode()
-    chores = [
-        "Cleaning the kitchen",
-        "Vacuuming or sweeping floors",
-        "Dusting and cleaning surfaces",
-        "Doing laundry",
-        "Cleaning bathrooms",
-        "Taking out the trash and recycling",
-        "Mowing the lawn",
-        "Maintaining the garden",
-        "Grocery shopping",
-        "Cooking and meal preparation",
-        "Cleaning windows and mirrors",
-        "Organizing and decluttering",
-        "Sweeping the porch or walkways",
-        "Cleaning the car(s)",
-        "Pet care"
-    ]
+    
 
     # Stuff for DEBUGGING
     names             = ['Emma', 'Olivia', 'Ava', 'Isabella', 'Sophia', 'Mia', 'Charlotte', 'Amelia', 'Harper', 'Evelyn', 'Abigail', 'Emily', 'Elizabeth', 'Avery', 'Sofia', 'Ella', 'Madison', 'Scarlett', 'Victoria', 'Aria', 'Grace', 'Chloe', 'Camila', 'Penelope', 'Riley', 'Nora', 'Lily', 'Eleanor', 'Hazel', 'Aubrey']
     importance_levels = ['not_important', 'somewhat_important', 'important', 'very_important']
     competence_levels = ["cant_do_it", "need_help", "can_do_it_easily"]
     comfort_levels    = ["hate_it", "dont_like_it", "neutral", "like_it", "love_it"]
-        
+    # Handle sending chores list 
     if app.debug == True:
         return render_template('survey.html', mode=mode, chores=chores, DEBUG=app.debug, random_name=random.choice(names), random_importance=random.choice(importance_levels), random_competence=random.choice(competence_levels), random_comfort=random.choice(comfort_levels))
     else:
