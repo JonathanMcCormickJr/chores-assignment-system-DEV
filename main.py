883c40e7491bde7061338543bc8707c55107f41a
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-28 00:39:31 +0000

    Still full of bugs for /analytics. Maybe I should overhaul the entire section.

--- main.py
+++ main.py
@@ -97,9 +97,12 @@ def analytics():
     # Initialize an empty dictionary to store task scores
     task_scores = defaultdict(float)
     for task in chores:
-        # Calculate the average score across all responses for this task
-        task_score = sum(response[1][task]["importance"] + response[1][task]["competence"] + response[1][task]["comfort"] for response in responses) / (3 * len(responses))
-        task_scores[task] = task_score
+        count = 0
+        for i in task:
+            # Calculate the average score across all responses for this task
+            task_score = sum(response[1][task][count] + response[1][task][count] + response[1][task][count] for response in responses) / (3 * len(responses))
+            task_scores[task] = task_score
+            count += 1
     
     # Sort task scores in descending order
     ranked_tasks = sorted(task_scores.items(), key=operator.itemgetter(1), reverse=True)
